d832bd2de41d3ec99bfe7b73c1372fd06f508ca6
balconyhotmilk <jimmyvo.05122002@gmail.com>
2024-05-22 13:37:35 +0700

    feat: set-up CIDv1 testing

--- client_test.go
+++ client_test.go
@@ -9,14 +9,46 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+const (
+	url      = "http://localhost:5001"
+	filePath = "test.jpg"
+)
+
 func TestAll(t *testing.T) {
 	clt, err := NewClient(&Config{
-		IpfsUrl:    "http://localhost:5001",
+		IpfsUrl:    url,
 		CIDVersion: 0,
 	})
 	require.NoError(t, err)
 
-	r, err := os.Open("test.jpg")
+	r, err := os.Open(filePath)
+	require.NoError(t, err)
+
+	ctx := context.Background()
+
+	added, err := clt.Add(ctx, r)
+	require.NoError(t, err)
+	fmt.Println(added.Hash)
+
+	ls, err := clt.ListCID(ctx)
+	require.NoError(t, err)
+
+	var cids []string
+	for cid := range ls {
+		cids = append(cids, cid.Hash)
+	}
+
+	require.Contains(t, cids, added.Hash)
+}
+
+func TestAllV1(t *testing.T) {
+	clt, err := NewClient(&Config{
+		IpfsUrl:    url,
+		CIDVersion: 1,
+	})
+	require.NoError(t, err)
+
+	r, err := os.Open(filePath)
 	require.NoError(t, err)
 
 	ctx := context.Background()
